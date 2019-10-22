feda31d7f5361d07b77f1462e220edd36019d3f4
Shraddha Bhuju <shraddha.bhuju@gmail.com>
2019-10-22 15:39:50 +0545

    LogIn and Register link and routing

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/LogIn', function(req, res, next) {
+  res.render('LogIn', { title: 'Express' });
+});
+router.get('/Register', function (req, res, next) {
   res.render('Register', { title: 'Express' });
 });
+
+
+
 
 module.exports = router;
